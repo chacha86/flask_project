9487f4fc6b273cca72838dbd8bd4e659c84f118b
chacha86 <taejincha0619@gmail.com>
2023-06-14 21:47:18 +0900

    플라스크 라우팅 함수 실행

---
+++ views/main_views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+bp = Blueprint('main', __name__, url_prefix='/main')
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+
+@bp.route('/')
+def index():
+    return 'Pybo index'
